950441250aa8b578d5073f397e13f82557fda460
elenapark <sungeun33@gmail.com>
2024-06-02 16:54:29 +0900

    feat: desktop용 organization sidebar 구현 - Shadcn-ui Accordion - uselocalstorage 훅을 이용하여 브라우저 리로드시에도 기존 According 상태 유지

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -1,4 +1,106 @@
+"use client";
+
+import { useEffect } from "react";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { useLocalStorage } from "usehooks-ts";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { NavItem, Organization } from "./NavItem";
+
+interface SidebarProps {
+  storageKey?: string;
+}
+
 // dashboard mobile navbar에서 사용 & organization sidebar에서 사용하므로 dashboard components에 생성
-export const Sidebar = () => {
-  return <div>God Damn Sidebar</div>;
+export const Sidebar = ({
+  storageKey = "trello-sidebar-key",
+}: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  // current organization
+  const {
+    organization: activeOrganization,
+    isLoaded: isActiveOrganizationLoaded,
+  } = useOrganization();
+
+  // existing organization list
+  const { userMemberships, isLoaded: isOrgListLoaded } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  const defaultAccordingValue = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
+  if (
+    !isOrgListLoaded ||
+    !isActiveOrganizationLoaded ||
+    userMemberships.isLoading
+  )
+    return <Skeleton />;
+
+  return (
+    <>
+      {/* Sidebar Header */}
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span className="pl-4">WorkSpaces</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          className="ml-auto" // same as justify-between
+          variant="ghost"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+
+      {/* Accordion */}
+      <Accordion
+        type="multiple"
+        className="space-y-2"
+        defaultValue={defaultAccordingValue}
+      >
+        {userMemberships.data?.map(({ organization }) => {
+          return (
+            <NavItem
+              key={organization.id}
+              organization={organization as Organization}
+              onExpand={onExpand}
+              isActive={activeOrganization?.id === organization.id}
+              isExpanded={expanded[organization.id]}
+            />
+          );
+        })}
+      </Accordion>
+    </>
+  );
 };
--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -7,10 +7,9 @@ export default function OrganizationLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-lime-200 pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto border-2 border-red-400">
-      this is OrganizationLayout
-      <div className=" bg-orange-500 flex gap-x-7">
-        <div className="bg-blue-300 w-64 shrink-0 hidden md:block">
+    <div className=" pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
+      <div className=" flex gap-x-7">
+        <div className="w-64 shrink-0 hidden md:block">
           <Sidebar />
         </div>
         {children}
