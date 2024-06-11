997383d2d8977a615c157cd518440cc2f8ef4674
elenapark <sungeun33@gmail.com>
2024-06-11 22:10:35 +0900

    feat: board list - get board list from database / board list ui & skeleton ui

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,8 +1,27 @@
+import Link from "next/link";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
 import { HelpCircle, User2 } from "lucide-react";
+import { db } from "@/lib/db";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export const BoardList = () => {
+export const BoardList = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect("/select-org");
+  }
+
+  const boards = await db.board.findMany({
+    where: {
+      orgId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <div className="space-y-4">
       <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -10,7 +29,21 @@ export const BoardList = () => {
         Your Boards
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            href={`/board/${board.id}`}
+            key={board.id}
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm
+            h-full w-full p-2 overflow-hidden
+            "
+          >
+            {/* layer */}
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative font-semibold text-white">{board.title}</p>
+          </Link>
+        ))}
         <FormPopover sideOffset={10} side={"right"}>
           <div
             role="button"
@@ -27,6 +60,28 @@ export const BoardList = () => {
             </Hint>
           </div>
         </FormPopover>
+      </div>
+    </div>
+  );
+};
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <User2 className="h-6 w-6 mr-2" />
+        Your Boards
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 ">
+        <Skeleton className="aspect-video h-18 w-32 p-2" />
+        <Skeleton className="aspect-video h-18 w-32 p-2" />
+        <Skeleton className="aspect-video h-18 w-32 p-2" />
+        <Skeleton className="aspect-video h-18 w-32 p-2" />
+        <Skeleton className="aspect-video h-18 w-32 p-2" />
+        <Skeleton className="aspect-video h-18 w-32 p-2" />
+        <Skeleton className="aspect-video h-18 w-32 p-2" />
+        <Skeleton className="aspect-video h-18 w-32 p-2" />
       </div>
     </div>
   );
--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -6,8 +6,8 @@ export default function OrganizationLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className=" pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
-      <div className=" flex gap-x-7">
+    <div className="pt-20 md:pt-24 px-4 max-w-7xl 2xl:max-w-screen-xl mx-auto">
+      <div className="flex gap-x-7">
         <div className="w-64 shrink-0 hidden md:block">
           <Sidebar />
         </div>
