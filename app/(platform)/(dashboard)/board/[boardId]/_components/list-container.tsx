1fdfd94158f9823bdee1397cfeeb09c10e924531
elenapark <sungeun33@gmail.com>
2024-06-15 15:40:31 +0900

    feat: Board List - model schema 생성 (list, card) - db에서 board list 조회 - list container 생성

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,8 @@
+interface ListContainerProps {
+  boardId: string;
+  data: any;
+}
+
+export const ListContainer = ({ boardId, data }: ListContainerProps) => {
+  return <div>컨테이너</div>;
+};
--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,13 +1,44 @@
-export default function BoardIdPage({
-  params,
-}: {
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
   params: {
     boardId: string;
   };
-}) {
+}
+
+export default async function BoardIdPage({ params }: BoardIdPageProps) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      // use board relation to filter by orgId
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
   return (
-    <div className="text-white font-semibold">
-      Board에 대한 컨텐츠가 여기에 들어감
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists} />
     </div>
   );
 }
