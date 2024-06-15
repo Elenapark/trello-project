1f7b4fc24625a869cf3637e603fd749564f3688e
elenapark <sungeun33@gmail.com>
2024-06-16 01:48:25 +0900

    WIP: DND 도중 index 문제로 보이는 버그 발견

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,19 +1,27 @@
 "use client";
 import { Card } from "@prisma/client";
+import { Draggable } from "@hello-pangea/dnd";
 
 interface CardProps {
   data: Card;
   index: number;
 }
 
-export const CardItem = ({ data }: CardProps) => {
+export const CardItem = ({ data, index }: CardProps) => {
   return (
-    <div
-      role="button" // clickable
-      className="truncate border-2 border-transparent hover:border-black *:
-  py-2 px-3 bg-white rounded-md text-sm shadow-sm"
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps} // activate the drag and drop
+          ref={provided.innerRef}
+          role="button" // clickable
+          className="truncate border-2 border-transparent hover:border-black *:
+   py-2 px-3 bg-white rounded-md text-sm shadow-sm"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   );
 };
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
 import { ListItem } from "./list-item";
@@ -8,6 +10,13 @@ import { ListItem } from "./list-item";
 interface ListContainerProps {
   boardId: string;
   data: ListWithCards[];
+}
+
+function reorder<T>(list: T[], startIndex: number, endIndex: number): T[] {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
 }
 
 export const ListContainer = ({ boardId, data }: ListContainerProps) => {
@@ -18,17 +27,128 @@ export const ListContainer = ({ boardId, data }: ListContainerProps) => {
     setOrderedData(data);
   }, [data]);
 
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result;
+
+    if (!destination) return;
+
+    // if dropped in the same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // User moves a list
+
+    if (type === "list") {
+      const result = reorder(orderedData, source.index, destination.index).map(
+        (item, idx) => ({
+          ...item,
+          order: idx,
+        })
+      );
+
+      // only update the UI (optimistic update)
+      setOrderedData(result);
+
+      // TODO: Trigger server action to update the DB
+    }
+
+    // User moves a card
+    if (type === "card") {
+      let newOrderedData = [...orderedData];
+
+      // card item을 drag하는 list를 찾는다
+      const sourceList = newOrderedData.find(
+        (list) => list.id === source.droppableId
+      );
+
+      // card item을 drop하는 list를 찾는다
+      const destList = newOrderedData.find(
+        (list) => list.id === destination.droppableId
+      );
+
+      if (!sourceList || !destList) return;
+
+      // check if cards exists in the source list
+      if (!sourceList.cards) {
+        sourceList.cards = [];
+      }
+
+      // check if cards exists in the destination list
+      if (!destList.cards) {
+        destList.cards = [];
+      }
+
+      // moving the card in the same list
+      if (source.droppableId === destination.droppableId) {
+        const reorderedCards = reorder(
+          sourceList.cards,
+          source.index,
+          destination.index
+        );
+
+        reorderedCards.forEach((card, idx) => {
+          card.order = idx;
+        });
+
+        sourceList.cards = reorderedCards;
+
+        setOrderedData(newOrderedData);
+        // TODO: Trigger server action to update the DB
+      } else {
+        // moving the card to another list
+
+        // remove the card from the source list
+        const [movedCard] = sourceList.cards.splice(source.index, 1);
+
+        if (!movedCard) return;
+        // assign the new listid (destination listid) to the moved card
+        movedCard.listId = destination.droppableId;
+
+        // insert the card to the destination list
+        destList.cards.splice(destination.index, 0, movedCard);
+
+        // source list cards reordering
+        sourceList.cards.forEach((card, idx) => {
+          card.order = idx;
+        });
+
+        // destination list cards reordering
+        destList.cards.forEach((card, idx) => {
+          card.order = idx;
+        });
+
+        setOrderedData(newOrderedData);
+        // TODO: Trigger server action to update the DB
+      }
+    }
+  };
+
   return (
-    <ol className="flex gap-x-3">
-      {orderedData.map((list, idx) => (
-        <ListItem key={list.id} index={idx} data={list} />
-      ))}
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3"
+          >
+            {orderedData.map((list, idx) => (
+              <ListItem key={list.id} index={idx} data={list} />
+            ))}
 
-      {/*  Add a new list form at the end of the list */}
-      <ListForm />
+            {/*  Add a new list form at the end of the list */}
+            {provided.placeholder}
+            <ListForm />
 
-      {/* empty space at the end of x-axis - create little bit of padding */}
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+            {/* empty space at the end of x-axis - create little bit of padding */}
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
