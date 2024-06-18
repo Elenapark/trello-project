4f1219ea266704ea06099053da0bba43fb5505c0
elenapark <sungeun33@gmail.com>
2024-06-18 21:32:44 +0900

    feat: Card Modal with update card action

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
+import useCardModal from "@/hooks/use-card-modal";
 
 interface CardProps {
   data: Card;
@@ -8,6 +9,8 @@ interface CardProps {
 }
 
 export const CardItem = ({ data, index }: CardProps) => {
+  const cardModal = useCardModal();
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -16,6 +19,7 @@ export const CardItem = ({ data, index }: CardProps) => {
           {...provided.dragHandleProps} // activate the drag and drop
           ref={provided.innerRef}
           role="button" // clickable
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black *:
    py-2 px-3 bg-white rounded-md text-sm shadow-sm"
         >
---
+++ components/modals/card-modal/header.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { ElementRef, useRef, useState } from "react";
+import { useParams } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
+import { Layout } from "lucide-react";
+
+import { FormInput } from "@/components/form/form-input";
+import { CardWithList } from "@/types";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useAction } from "@/hooks/use-action";
+import { updateCard } from "@/actions/update-card";
+import { toast } from "sonner";
+
+interface HeaderProps {
+  data: CardWithList;
+}
+
+export const Header = ({ data }: HeaderProps) => {
+  const queryClient = useQueryClient();
+  const params = useParams();
+
+  const inputRef = useRef<ElementRef<"input">>(null);
+  const [title, setTitle] = useState(data.title);
+
+  const { execute } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["card", data.id],
+      });
+      toast.success(`Card "${data.title}" updated successfully`);
+    },
+    onError: (err) => {
+      toast.error(err);
+    },
+  });
+
+  const onBlur = () => {
+    inputRef.current?.form?.requestSubmit();
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = params.boardId as string;
+
+    if (title === data.title) {
+      return;
+    }
+
+    execute({
+      title,
+      id: data.id,
+      boardId,
+    });
+  };
+
+  return (
+    <div className="flex items-center gap-x-3 mb-5">
+      <Layout className="h-5 w-5 mt-1 text-neutral-700" />
+      <div className="w-full">
+        <form action={onSubmit}>
+          <FormInput
+            id="title"
+            ref={inputRef}
+            onBlur={onBlur}
+            defaultValue={title}
+            className="font-semibold text-xl px-1 bg-transparent border-transparent releative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
+          />
+        </form>
+        <p className="text-sm text-muted-foreground">
+          in list <span className="underline">{data.list.title}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+Header.Skeleton = function HeaderSkeleteon() {
+  return (
+    <div className="flex items-start gap-x-3 mb-6">
+      <Skeleton className="h-6 w-6 mt-1 bg-neutral-200" />
+      <div>
+        <Skeleton className="h-6 w-24 mb-1 bg-neutral-200" />
+        <Skeleton className="h-4 w-12 mb-1 bg-neutral-200" />
+      </div>
+    </div>
+  );
+};
